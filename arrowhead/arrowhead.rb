c5e6b815c181acb885beb8ac62442dce64059be6
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-01-31 21:18:29 -0500

    Put arror closer

--- arrowhead/arrowhead.rb
+++ arrowhead/arrowhead.rb
@@ -19,16 +19,21 @@ class Arrowhead
   def self.classify(region, shape)
     if CLASSIFICATIONS.include? region
       shapes = CLASSIFICATIONS[region]
-      if shapes.include? shape
-        arrowhead = shapes[shape]
-        "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
-      else
-        raise "Unknown shape value. Are you sure you know what you're talking about?"
-      end
+      shape_statement(shapes, shape)
     else
       raise "Unknown region, please provide a valid region."
     end
   end
+
+  def self.shape_statement(shapes, shape)
+    if shapes.include? shape
+      arrowhead = shapes[shape]
+      "You have a(n) '#{arrowhead}' arrowhead. Probably priceless."
+    else
+      raise "Unknown shape value. Are you sure you know what you're talking about?"
+    end
+  end
+
 end
 
 puts Arrowhead.classify(:northern_plains, :bifurcated)
